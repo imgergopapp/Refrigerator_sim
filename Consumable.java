f9197c16121c3a8b809b7d53e1554301f8bf4580
Gergő Papp <imgergopapp@gmail.com>
2019-01-25 21:38:56 +0100

    added base classes and gitignore

---
+++ Consumable.java
@@ -0,0 +1,21 @@
+public class Consumable{
+
+    //Instance Fields
+    String name;
+    String bestBefore;
+    int calories;
+    boolean isEdible;
+
+    //Constructor
+    public Consumable(String name, String bestBefore, int calories, Boolean isEdible){
+        this.name = name;
+        this.bestBefore = bestBefore;
+        this.calories = calories;
+        this.isEdible = isEdible;
+    }
+
+    //Methods
+    
+
+
+}
