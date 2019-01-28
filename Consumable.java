4ce958037d23c3ddf3a74f1f54d410580061fca3
Gergő Papp <imgergopapp@gmail.com>
2019-01-28 18:03:52 +0100

    added Getters and Setters

--- Consumable.java
+++ Consumable.java
@@ -1,10 +1,10 @@
 public class Consumable{
 
     //Instance Fields
-    String name;
-    String bestBefore;
-    int calories;
-    boolean isEdible;
+    private String name;
+    private String bestBefore;
+    private int calories;
+    private boolean isEdible;
 
     //Constructor
     public Consumable(String name, String bestBefore, int calories, Boolean isEdible){
@@ -14,8 +14,39 @@ public class Consumable{
         this.isEdible = isEdible;
     }
 
-    //Methods
+    //Methods - Setters:
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public void setBestBefore(String newBestBefore){
+        this.bestBefore = newBestBefore;
+    }
+
+    public void setCalories(int newCalories){
+        this.calories = newCalories;
+    }
+
+    public void setIsEdible(boolean newIsEdible){
+        this.isEdible = newIsEdible;
+    }
     
+    //Methods - Getters:
 
+    public String getName(){
+        return this.name;
+    }
 
+    public String getBestBefore(){
+        return this.bestBefore;
+    }
+
+    public int getCalories(){
+        return this.calories;
+    }
+
+    public boolean getIsEdible(){
+        return this.isEdible;
+    }
 }
