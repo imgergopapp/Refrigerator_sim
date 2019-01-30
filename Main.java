29b8e50c0b46cfdec06541a8db29d2ef1efcf4a8
Gergő Papp <imgergopapp@gmail.com>
2019-01-30 21:45:12 +0100

    ArrayList for Consumables

--- Main.java
+++ Main.java
@@ -1,18 +1,21 @@
+import java.util.ArrayList;
+
 public class Main{
+    
+
     public static void main(String[] args){
 
+        ArrayList<Consumable> fridgeElements = new ArrayList<Consumable>();
         Fridge refridgerator = new Fridge();
 
-        refridgerator.apple.isEdible();
-        refridgerator.paprika.isEdible();
-        refridgerator.jam.isEdible();
-        refridgerator.chocolate.isEdible();
-
         
-       
+        fridgeElements.add(refridgerator.apple);
+        fridgeElements.add(refridgerator.chocolate);
+        
 
-       
-
+        for (Consumable consumables : fridgeElements){
+            System.out.println(consumables.getName());
+        }
     }
 
 }
