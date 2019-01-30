bc2dbcc19ab8d2d6523c8a3025a4c24c040aadfe
Gergő Papp <imgergopapp@gmail.com>
2019-01-30 21:44:08 +0100

    added isOpen method with Getter & Setter

--- Fridge.java
+++ Fridge.java
@@ -1,4 +1,8 @@
+import java.util.*;
+
 public class Fridge{
+
+        private boolean isOpen;
 
         Food paprika = new Food("chili paprika", "01.27.2019", 89, true);
         Food jam = new Food("Strawberry jam", "10.08.2020", 320, false);
@@ -7,4 +11,13 @@ public class Fridge{
 
         Drink beer = new Drink("Beer", "02.22.2022", 156, true);
 
+        //Methods - Setters:
+        public void SetIsOpen(boolean isOpen){
+            this.isOpen = isOpen;
+        }
+
+         //Methods - Getters:
+        public boolean GetIsOpen(){
+            return this.isOpen;
+        }
 }
