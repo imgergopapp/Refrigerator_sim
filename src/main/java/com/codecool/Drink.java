956f52bd38dfb2c8dbbc9cc642ea4e73cd96412b
Gergő Papp <imgergopapp@gmail.com>
2019-02-18 18:04:23 +0100

    Added xmlRead method

--- src/main/java/com/codecool/Drink.java
+++ src/main/java/com/codecool/Drink.java
@@ -1,7 +1,16 @@
 package com.codecool;
 
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
 public class Drink extends Consumable{
+
+    private static File xmlFile = new File("src/data/drinks.xml");
 
     private boolean isAlcoholic;
     private boolean isFizzy;
@@ -12,7 +21,25 @@ public class Drink extends Consumable{
         this.isFizzy = isFizzy;
     }
 
-    //Methods
+    //Xml Methods :
+    public static Drink[] readXml() throws  Exception{
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(xmlFile);
+        NodeList nList = document.getElementsByTagName("Drink");
+        Drink[] drinks = new Drink[nList.getLength()] ;
+
+        for (int i = 0; i < nList.getLength(); i++){
+            String name = document.getElementsByTagName("name").item(i).getTextContent();
+            String bestBefore = document.getElementsByTagName("bestbefore").item(i).getTextContent();
+            int calories = Integer.valueOf(document.getElementsByTagName("calories").item(i).getTextContent());
+            boolean isalcoholic = Boolean.parseBoolean (document.getElementsByTagName("isalcoholic").item(i).getTextContent());
+            boolean isFizzy = Boolean.parseBoolean (document.getElementsByTagName("isfizzy").item(i).getTextContent());
+            Drink drink = new Drink(name,bestBefore,calories,isalcoholic,isFizzy);
+            drinks[i] = drink;
+        }
+        return drinks;
+    }
 
     
 }
--- src/main/java/com/codecool/Food.java
+++ src/main/java/com/codecool/Food.java
@@ -1,6 +1,15 @@
 package com.codecool;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
 public class Food extends Consumable{
+
+    private static File xmlFile = new File("src/data/foods.xml");
+
 
     private boolean isSpicy;
 
@@ -8,5 +17,25 @@ public class Food extends Consumable{
         super(name, bestBefore, calories);
         this.isSpicy = isSpicy;
     }
+
+    //Xml Methods :
+    public static Food[] readXml() throws  Exception{
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(xmlFile);
+        NodeList nList = document.getElementsByTagName("Food");
+        Food[] foods = new Food[nList.getLength()] ;
+
+        for (int i = 0; i < nList.getLength(); i++){
+            String name = document.getElementsByTagName("name").item(i).getTextContent();
+            String bestBefore = document.getElementsByTagName("bestbefore").item(i).getTextContent();
+            int calories = Integer.valueOf(document.getElementsByTagName("calories").item(i).getTextContent());
+            boolean isSpicy = Boolean.parseBoolean (document.getElementsByTagName("isspicy").item(i).getTextContent());
+            Food food = new Food(name,bestBefore,calories,isSpicy);
+            foods[i] = food;
+        }
+        return foods;
+    }
+
 
 }
