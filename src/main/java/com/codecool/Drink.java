2e2c4ec07e24ac4ebd0b20f28409bd11233d032f
Gergő Papp <imgergopapp@gmail.com>
2019-02-18 21:24:32 +0100

    added writeXml, toString & makeConsumable methods

--- src/main/java/com/codecool/Drink.java
+++ src/main/java/com/codecool/Drink.java
@@ -2,10 +2,15 @@ package com.codecool;
 
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 
 public class Drink extends Consumable{
@@ -21,11 +26,42 @@ public class Drink extends Consumable{
         this.isFizzy = isFizzy;
     }
 
+    public static String[] makeConsumable(){
+
+        String name = Input.getInputString("Drink's name: ");
+
+        String month = String.valueOf(Input.getInputInt("Best before - Month: ",1,12));
+        String day = String.valueOf(Input.getInputInt("Best before - Day: ",1,28));
+        String year = String.valueOf(Input.getInputInt("Best before - Year: ",2000,2030));
+        String bestBefore = month + day + year;
+
+        String calories = String.valueOf(Input.getInputInt("Calories: "));
+        String isalcoholic = Input.getInputString("Is alcoholic: ");
+        String isfizzy = Input.getInputString("Is fizzy: ");
+
+        return new String[]{name,bestBefore,calories,isalcoholic,isfizzy};
+    }
+
+    @Override
+    public String toString() {
+        return "Drink's name: " + getName() + " | " +
+            "Best before: " + getBestBefore() + " | " +
+            "Calories: " + getCalories() + " | " +
+            "isAlcoholic: " + isAlcoholic + " | " +
+            "isFizzy: " + isFizzy;
+    }
+
     //Xml Methods :
-    public static Drink[] readXml() throws  Exception{
+    public static Drink[] readXml(){
+        Document document = null;
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        Document document = documentBuilder.parse(xmlFile);
+        try{
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            document = documentBuilder.parse(xmlFile);
+        }
+        catch (Exception e){
+
+        }
         NodeList nList = document.getElementsByTagName("Drink");
         Drink[] drinks = new Drink[nList.getLength()] ;
 
@@ -39,6 +75,56 @@ public class Drink extends Consumable{
             drinks[i] = drink;
         }
         return drinks;
+    }
+
+
+
+    public static void writeXml(String[] drinkProperties){
+        Document document = null;
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        try{
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            document = documentBuilder.parse(xmlFile);
+        }
+        catch (Exception e){
+
+        }
+
+        Element root = document.getDocumentElement();
+        Element drink = document.createElement("Drink");
+
+        root.appendChild(drink);
+
+        Element name = document.createElement("name");
+        Element bestbefore = document.createElement("bestbefore");
+        Element calories = document.createElement("calories");
+        Element isAlcoholic = document.createElement("isalcoholic");
+        Element isFizzy = document.createElement("isfizzy");
+
+        name.appendChild(document.createTextNode(drinkProperties[0]));
+        bestbefore.appendChild(document.createTextNode(drinkProperties[1]));
+        calories.appendChild(document.createTextNode(drinkProperties[2]));
+        isAlcoholic.appendChild(document.createTextNode(drinkProperties[3]));
+        isFizzy.appendChild(document.createTextNode(drinkProperties[4]));
+
+        drink.appendChild(name);
+        drink.appendChild(bestbefore);
+        drink.appendChild(calories);
+        drink.appendChild(isAlcoholic);
+        drink.appendChild(isFizzy);
+
+        TransformerFactory factory = TransformerFactory.newInstance();
+        DOMSource domSource = new DOMSource(document);
+        StreamResult streamResult = new StreamResult(new File("src/data/foods.xml"));
+        try{
+            Transformer transformer = factory.newTransformer();
+            transformer.transform(domSource, streamResult);
+
+        }
+        catch (Exception e){
+
+        }
+
     }
 
     
