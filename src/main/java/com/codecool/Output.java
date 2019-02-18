47955a6ad64626070eb71d8907962af16a3eb807
Gergő Papp <imgergopapp@gmail.com>
2019-02-18 18:55:23 +0100

    print formatted

--- src/main/java/com/codecool/Output.java
+++ src/main/java/com/codecool/Output.java
@@ -2,9 +2,9 @@ package com.codecool;
 
 public class Output{
     
-    public void printMenu(String[] menuOptions){
+    public static void printMenu(String[] menuOptions){
         for (int i = 0; i < menuOptions.length; i++){
-            System.out.println(Integer.toString(i) + ". - " + menuOptions[i]);
+            System.out.println(Integer.toString(i) + ". " + menuOptions[i]);
         }
 
     }
