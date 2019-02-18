dae0479a3a706e419981be4b44c2211c8827793f
Gergő Papp <imgergopapp@gmail.com>
2019-02-18 17:03:53 +0100

    Added isFizzy instance field

--- src/main/java/com/codecool/Drink.java
+++ src/main/java/com/codecool/Drink.java
@@ -4,10 +4,12 @@ package com.codecool;
 public class Drink extends Consumable{
 
     private boolean isAlcoholic;
+    private boolean isFizzy;
 
-    public Drink(String name, String bestBefore, int calories, boolean isAlcoholic){
+    public Drink(String name, String bestBefore, int calories, boolean isAlcoholic, boolean isFizzy){
         super(name, bestBefore, calories);
         this.isAlcoholic = isAlcoholic;
+        this.isFizzy = isFizzy;
     }
 
     //Methods
