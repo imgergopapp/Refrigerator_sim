3d1567ed90904992288768f15b1eca53b33b01ea
Gergő Papp <imgergopapp@gmail.com>
2019-02-23 12:09:12 +0100

    moved Xml functions to XmlParser & reformat

--- src/main/java/com/codecool/Drink.java
+++ src/main/java/com/codecool/Drink.java
@@ -1,22 +1,6 @@
 package com.codecool;
 
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.File;
-
 public class Drink extends Consumable{
-
-    private static File xmlFile = new File("src/data/drinks.xml");
 
     private boolean isAlcoholic;
     private boolean isFizzy;
@@ -34,7 +18,7 @@ public class Drink extends Consumable{
         String month = String.valueOf(Input.getInputInt("Best before - Month: ",1,12));
         String day = String.valueOf(Input.getInputInt("Best before - Day: ",1,28));
         String year = String.valueOf(Input.getInputInt("Best before - Year: ",2000,2030));
-        String bestBefore = month + day + year;
+        String bestBefore = month + "." + day + "." + year;
 
         String calories = String.valueOf(Input.getInputInt("Calories: "));
         String isalcoholic = Input.getInputString("Is alcoholic: ");
@@ -51,85 +35,4 @@ public class Drink extends Consumable{
             "isAlcoholic: " + isAlcoholic + " | " +
             "isFizzy: " + isFizzy;
     }
-
-    //Xml Methods :
-    public static Drink[] readXml(){
-        Document document = null;
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        try{
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            document = documentBuilder.parse(xmlFile);
-        }
-        catch (Exception e){
-
-        }
-        NodeList nList = document.getElementsByTagName("Drink");
-        Drink[] drinks = new Drink[nList.getLength()] ;
-
-        for (int i = 0; i < nList.getLength(); i++){
-            String name = document.getElementsByTagName("name").item(i).getTextContent();
-            String bestBefore = document.getElementsByTagName("bestbefore").item(i).getTextContent();
-            int calories = Integer.valueOf(document.getElementsByTagName("calories").item(i).getTextContent());
-            boolean isalcoholic = Boolean.parseBoolean (document.getElementsByTagName("isalcoholic").item(i).getTextContent());
-            boolean isFizzy = Boolean.parseBoolean (document.getElementsByTagName("isfizzy").item(i).getTextContent());
-            Drink drink = new Drink(name,bestBefore,calories,isalcoholic,isFizzy);
-            drinks[i] = drink;
-        }
-        return drinks;
-    }
-
-
-
-    public static void writeXml(String[] drinkProperties){
-        Document document = null;
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        try{
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            document = documentBuilder.parse(xmlFile);
-        }
-        catch (Exception e){
-
-        }
-
-        Element root = document.getDocumentElement();
-        Element drink = document.createElement("Drink");
-
-        root.appendChild(drink);
-
-        Element name = document.createElement("name");
-        Element bestbefore = document.createElement("bestbefore");
-        Element calories = document.createElement("calories");
-        Element isAlcoholic = document.createElement("isalcoholic");
-        Element isFizzy = document.createElement("isfizzy");
-
-        name.appendChild(document.createTextNode(drinkProperties[0]));
-        bestbefore.appendChild(document.createTextNode(drinkProperties[1]));
-        calories.appendChild(document.createTextNode(drinkProperties[2]));
-        isAlcoholic.appendChild(document.createTextNode(drinkProperties[3]));
-        isFizzy.appendChild(document.createTextNode(drinkProperties[4]));
-
-        drink.appendChild(name);
-        drink.appendChild(bestbefore);
-        drink.appendChild(calories);
-        drink.appendChild(isAlcoholic);
-        drink.appendChild(isFizzy);
-
-        TransformerFactory factory = TransformerFactory.newInstance();
-        DOMSource domSource = new DOMSource(document);
-        StreamResult streamResult = new StreamResult(new File("src/data/drinks.xml"));
-        try{
-            Transformer transformer = factory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-            transformer.transform(domSource, streamResult);
-
-        }
-        catch (Exception e){
-
-        }
-
-    }
-
-    
 }
--- src/main/java/com/codecool/Food.java
+++ src/main/java/com/codecool/Food.java
@@ -1,23 +1,6 @@
 package com.codecool;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-
-import javax.sql.rowset.spi.XmlReader;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.File;
-
 public class Food extends Consumable {
-
-    private static File xmlFile = new File("src/data/foods.xml");
-
 
     private boolean isSpicy;
 
@@ -33,7 +16,7 @@ public class Food extends Consumable {
         String month = String.valueOf(Input.getInputInt("Best before - Month: ",1,12));
         String day = String.valueOf(Input.getInputInt("Best before - Day: ",1,28));
         String year = String.valueOf(Input.getInputInt("Best before - Year: ",2000,2030));
-        String bestBefore = month + day + year;
+        String bestBefore = month + "." + day + "." + year;
 
         String calories = String.valueOf(Input.getInputInt("Calories: "));
         String isSpicy = Input.getInputString("Is spicy: ");
@@ -47,80 +30,5 @@ public class Food extends Consumable {
             "Best before: " + getBestBefore() + " | " +
             "Calories: " + getCalories() + " | " +
             "Is spicy: " + isSpicy;
-    }
-
-    //Xml Methods :
-    public static Food[] readXml(){
-        Document document = null;
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        try{
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            document = documentBuilder.parse(xmlFile);
-        }
-        catch (Exception e){
-
-        }
-
-        NodeList nList = document.getElementsByTagName("Food");
-        Food[] foods = new Food[nList.getLength()] ;
-
-        for (int i = 0; i < nList.getLength(); i++){
-            String name = document.getElementsByTagName("name").item(i).getTextContent();
-            String bestBefore = document.getElementsByTagName("bestbefore").item(i).getTextContent();
-            int calories = Integer.valueOf(document.getElementsByTagName("calories").item(i).getTextContent());
-            boolean isSpicy = Boolean.parseBoolean (document.getElementsByTagName("isspicy").item(i).getTextContent());
-            Food food = new Food(name,bestBefore,calories,isSpicy);
-            foods[i] = food;
-        }
-        return foods;
-    }
-
-    public static void writeXml(String[] foodProperties){
-        Document document = null;
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        try{
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            document = documentBuilder.parse(xmlFile);
-        }
-        catch (Exception e){
-
-        }
-
-        Element root = document.getDocumentElement();
-        Element food = document.createElement("Food");
-
-        root.appendChild(food);
-
-        Element name = document.createElement("name");
-        Element bestbefore = document.createElement("bestbefore");
-        Element calories = document.createElement("calories");
-        Element isspicy = document.createElement("isspicy");
-
-        name.appendChild(document.createTextNode(foodProperties[0]));
-        bestbefore.appendChild(document.createTextNode(foodProperties[1]));
-        calories.appendChild(document.createTextNode(foodProperties[2]));
-        isspicy.appendChild(document.createTextNode(foodProperties[3]));
-
-        food.appendChild(name);
-        food.appendChild(bestbefore);
-        food.appendChild(calories);
-        food.appendChild(isspicy);
-
-        TransformerFactory factory = TransformerFactory.newInstance();
-
-        DOMSource domSource = new DOMSource(document);
-        StreamResult streamResult = new StreamResult(new File("src/data/foods.xml"));
-        try{
-            Transformer transformer = factory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-            transformer.transform(domSource, streamResult);
-
-        }
-        catch (Exception e){
-
-        }
-
     }
 }
