5fb98aa10887b572f94aab2f3030583bad4000fb
Gergő Papp <imgergopapp@gmail.com>
2019-02-23 20:54:27 +0100

    output formatted with new lines

--- src/main/java/com/codecool/Input.java
+++ src/main/java/com/codecool/Input.java
@@ -3,12 +3,12 @@ package com.codecool;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Input{
+public class Input {
 
-    public static int getInputInt(String message,int rangeFrom, int rangeTo){
+    public static int getInputInt(String message, int rangeFrom, int rangeTo) {
 
         int input = 0;
-        while(true) {
+        while (true) {
             Scanner reader = new Scanner(System.in);
             System.out.print("\n" + message);
             try {
@@ -19,8 +19,7 @@ public class Input{
                 } else {
                     System.out.println(input + " is not a number between " + rangeFrom + " and " + rangeTo + " !");
                 }
-            }
-            catch (InputMismatchException e) { //handle exp
+            } catch (InputMismatchException e) { //handle exp
                 System.out.println("Input MUST only contain numbers!");
             }
         }
@@ -28,68 +27,74 @@ public class Input{
     }
 
     //Method overload
-    public static int getInputInt(String message){
+    public static int getInputInt(String message) {
         int input = 0;
-        while(true) {
+        while (true) {
             Scanner reader = new Scanner(System.in);
             System.out.print("\n" + message);
             try {
                 input = reader.nextInt();
                 reader.nextLine();
                 break;
-            }
-            catch (InputMismatchException e) { //handle exp
+            } catch (InputMismatchException e) { //handle exp
                 System.out.println("Input MUST only contain numbers!");
             }
         }
+        System.out.println();
         return input;
     }
 
-    public static String getInputString(String message, String[] expectedInput){
+    public static String getInputString(String message, String[] expectedInput) {
+
+        String validInputs = "";
+        for (int i = 0; i < expectedInput.length; i++) {
+            validInputs += expectedInput[i] + " ";
+        }
 
         String input = null;
-        while(true){
+        while (true) {
+
+            System.out.print("Valid inputs: " + validInputs + "\n" + message);
+
             Scanner reader = new Scanner(System.in);
-            System.out.print(message);
             input = reader.nextLine();
             System.out.println();
 
-            if(isAlphabetic(input)) {
-                for(String word : expectedInput){
-                    if (word.toLowerCase().equals(input.toLowerCase())){
+            if (isAlphabetic(input)) {
+                for (String word : expectedInput) {
+                    if (word.toLowerCase().equals(input.toLowerCase())) {
                         return input.toLowerCase();
                     }
                 }
-            }
-            else {
+                System.out.println(input + " is NOT a valid input!\n");
+            } else {
                 System.out.println("Input MUST contain only letters!");
             }
         }
     }
 
     //Method overload :
-    public static String getInputString(String message){
+    public static String getInputString(String message) {
 
         String input = null;
-        while(true){
+        while (true) {
             Scanner reader = new Scanner(System.in);
             System.out.print(message);
             input = reader.nextLine();
             System.out.println();
 
-            if(isAlphabetic(input)) {
+            if (isAlphabetic(input)) {
                 return input.toLowerCase();
-            }
-            else {
+            } else {
                 System.out.println("Input MUST contain only letters!");
             }
         }
     }
 
-    private static boolean isAlphabetic(String word){
+    private static boolean isAlphabetic(String word) {
         char[] charArray = word.toCharArray();
-        for (char charachters : charArray){
-            if (!Character.isLetter(charachters)){
+        for (char charachters : charArray) {
+            if (!Character.isLetter(charachters)) {
                 return false;
             }
         }
