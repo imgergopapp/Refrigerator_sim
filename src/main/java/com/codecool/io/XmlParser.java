3c3c854e6b7d536e0c449011acc661be0d048ba5
Gergő Papp <imgergopapp@gmail.com>
2019-03-02 22:24:46 +0100

    Added ConsumableType enum compatibility

--- src/main/java/com/codecool/io/XmlParser.java
+++ src/main/java/com/codecool/io/XmlParser.java
@@ -1,6 +1,7 @@
 package com.codecool.io;
 
 import com.codecool.main.Consumable;
+import com.codecool.main.ConsumableType;
 import com.codecool.main.Drink;
 import com.codecool.main.Food;
 import org.w3c.dom.Document;
@@ -25,48 +26,46 @@ public class XmlParser {
     private static File xmlFile;
 
     // Creating Document type obj.
-    private static Document createDocument(String category){
-        xmlFile = new File("src/data/" + category + "s.xml") ;
+    private static Document createDocument(String category) {
+        xmlFile = new File("src/data/" + category + "s.xml");
         Document document = null;
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        try{
+        try {
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
             document = documentBuilder.parse(xmlFile);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
 
         }
         return document;
     }
 
     // Write the document to the XMl
-    private static void save(Document document, String category){
+    private static void save(Document document, String category) {
         TransformerFactory factory = TransformerFactory.newInstance();
 
         Document documentClean = removeEmptyNodes(document);
         DOMSource domSource = new DOMSource(documentClean);
         StreamResult streamResult = new StreamResult(new File("src/data/" + category + "s.xml"));
-        try{
+        try {
             Transformer transformer = factory.newTransformer();
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
             transformer.setOutputProperty(OutputKeys.METHOD, "xml");
             transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
             transformer.transform(domSource, streamResult);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
 
         }
     }
 
     //Delete from the Xml
     public static void delete(String consumableName, String category) {
-       Document document = createDocument(category);
-       String node = category.equals("food") ? "Food" : "Drink";
+        Document document = createDocument(category);
+        String node = category.equals("food") ? "Food" : "Drink";
 
         NodeList nList = document.getElementsByTagName(node);
         for (int i = 0; i < nList.getLength(); i++) {
-            Element consumable = (Element)nList.item(i);
-            Element name = (Element)consumable.getElementsByTagName("name").item(0);
+            Element consumable = (Element) nList.item(i);
+            Element name = (Element) consumable.getElementsByTagName("name").item(0);
             String currentName = name.getTextContent();
             if (currentName.equals(consumableName)) {
 
@@ -74,7 +73,7 @@ public class XmlParser {
                 break;
             }
         }
-        save(document,category);
+        save(document, category);
     }
 
     //Update
@@ -84,47 +83,46 @@ public class XmlParser {
 
         NodeList nList = document.getElementsByTagName(node);
         for (int i = 0; i < nList.getLength(); i++) {
-            Element consumable = (Element)nList.item(i);
-            Element name = (Element)consumable.getElementsByTagName("name").item(0);
+            Element consumable = (Element) nList.item(i);
+            Element name = (Element) consumable.getElementsByTagName("name").item(0);
             String currentName = name.getTextContent();
             if (currentName.equals(originalName)) {
 
-               consumable.getElementsByTagName("name").item(0).setTextContent(newProperties[0]);
-               consumable.getElementsByTagName("bestbefore").item(0).setTextContent(newProperties[1]);
-               consumable.getElementsByTagName("calories").item(0).setTextContent(newProperties[2]);
+                consumable.getElementsByTagName("name").item(0).setTextContent(newProperties[0]);
+                consumable.getElementsByTagName("bestbefore").item(0).setTextContent(newProperties[1]);
+                consumable.getElementsByTagName("calories").item(0).setTextContent(newProperties[2]);
 
-               if(category.equals("food")){
-                   consumable.getElementsByTagName("isspicy").item(0).setTextContent(newProperties[3]);
-               }
-               else{
-                   consumable.getElementsByTagName("isalcoholic").item(0).setTextContent(newProperties[3]);
-                   consumable.getElementsByTagName("isfizzy").item(0).setTextContent(newProperties[4]);
-               }
+                if (category.equals("food")) {
+                    consumable.getElementsByTagName("isspicy").item(0).setTextContent(newProperties[3]);
+                } else {
+                    consumable.getElementsByTagName("isalcoholic").item(0).setTextContent(newProperties[3]);
+                    consumable.getElementsByTagName("isfizzy").item(0).setTextContent(newProperties[4]);
+                }
             }
         }
-        save(document,category);
+        save(document, category);
     }
 
     //Read Xml & return with it's items
-    public static Consumable[] read(String category){
+    public static Consumable[] read(String category) {
         Document document = createDocument(category);
         String node = category.equals("food") ? "Food" : "Drink";
+        ConsumableType type = category.equals("food") ? ConsumableType.FOOD : ConsumableType.DRINK;
         NodeList nList = document.getElementsByTagName(node);
-        Consumable[] consumables = new Consumable[nList.getLength()] ;
+        Consumable[] consumables = new Consumable[nList.getLength()];
 
-            for (int i = 0; i < nList.getLength(); i++){
+        for (int i = 0; i < nList.getLength(); i++) {
             String name = document.getElementsByTagName("name").item(i).getTextContent();
             String bestBefore = document.getElementsByTagName("bestbefore").item(i).getTextContent();
             int calories = Integer.valueOf(document.getElementsByTagName("calories").item(i).getTextContent());
-            if (node.equals("Food")){
-                boolean isSpicy = Boolean.parseBoolean (document.getElementsByTagName("isspicy").item(i).getTextContent());
-                Food food = new Food(name,bestBefore,calories,isSpicy);
+            if (node.equals("Food")) {
+                boolean isSpicy = Boolean.parseBoolean(document.getElementsByTagName("isspicy").item(i).getTextContent());
+                Food food = new Food(name, bestBefore, calories, type, isSpicy);
                 consumables[i] = food;
-            }
-            else{
-                boolean isalcoholic = Boolean.parseBoolean (document.getElementsByTagName("isalcoholic").item(i).getTextContent());
-                boolean isFizzy = Boolean.parseBoolean (document.getElementsByTagName("isfizzy").item(i).getTextContent());
-                Drink drink = new Drink(name,bestBefore,calories,isalcoholic,isFizzy);
+            } else {
+                boolean isalcoholic = Boolean.parseBoolean(document.getElementsByTagName("isalcoholic").item(i).getTextContent());
+                boolean isFizzy = Boolean.parseBoolean(document.getElementsByTagName("isfizzy").item(i).getTextContent());
+                Drink drink = new Drink(name, bestBefore, calories, type, isalcoholic, isFizzy);
                 consumables[i] = drink;
             }
         }
@@ -153,12 +151,11 @@ public class XmlParser {
         consumable.appendChild(bestbefore);
         consumable.appendChild(calories);
 
-        if (node.equals("Food")){
+        if (node.equals("Food")) {
             Element isspicy = document.createElement("isspicy");
             isspicy.appendChild(document.createTextNode(properties[3]));
             consumable.appendChild(isspicy);
-        }
-        else {
+        } else {
             Element isAlcoholic = document.createElement("isalcoholic");
             Element isFizzy = document.createElement("isfizzy");
             isAlcoholic.appendChild(document.createTextNode(properties[3]));
@@ -166,10 +163,10 @@ public class XmlParser {
             consumable.appendChild(isAlcoholic);
             consumable.appendChild(isFizzy);
         }
-        save(document,category);
+        save(document, category);
     }
 
-    public static Document removeEmptyNodes(Document document) {
+    private static Document removeEmptyNodes(Document document) {
         XPath xp = XPathFactory.newInstance().newXPath();
         NodeList nl = null;
         try {
