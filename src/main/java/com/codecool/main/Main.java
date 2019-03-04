7a17fc02efb1d1d163160bfe79884528f83f15c4
Gergő Papp <imgergopapp@gmail.com>
2019-03-04 20:50:37 +0100

    Compare items added to menu

--- src/main/java/com/codecool/main/Main.java
+++ src/main/java/com/codecool/main/Main.java
@@ -4,6 +4,8 @@ import com.codecool.io.Input;
 import com.codecool.io.Output;
 import com.codecool.io.XmlParser;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,6 +15,7 @@ public class Main {
 
             String[] menuTitles = {"[Exit] Close application",
                 "[Read] Show whats in the fridge",
+                "[Compare] Order items by Date (and Name)",
                 "[Find] Is consumable",
                 "[Create] Add consumable",
                 "[Update] Change consumable",
@@ -37,18 +40,23 @@ public class Main {
                     Output.showFridgeElements(fridgeElements);
                     break;
                 case 2:
+                    System.out.println("\n- [Compare] Sort items by Date (and Name) -\n");
+                    Arrays.sort(fridgeElements,new DateSort());
+                    Output.showFridgeElements(fridgeElements);
+                    break;
+                case 3:
                     System.out.println("\n- [Find] Is consumable -\n");
                     name = Input.getInputString("Name of the consumable: ", validNames);
                     Consumable consumable = smartFridge.findConsumable(name);
                     consumable.isEdible();
                     break;
-                case 3:
+                case 4:
                     System.out.println("\n- [Create] Add consumable -\n");
                     String userChoice = Input.getInputString("Choose between food and drink: ", validType);
                     String[] properties = userChoice.equals("food") ? Food.getProperties() : Drink.getProperties();
                     XmlParser.write(properties, userChoice);
                     break;
-                case 4:
+                case 5:
                     System.out.println("\n- [Update] Change consumable -\n");
                     userChoice = Input.getInputString("Choose between food and drink: ", validType);
                     validNames = smartFridge.getFridgeItemNames(userChoice);
@@ -56,7 +64,7 @@ public class Main {
                     properties = userChoice.equals("food") ? Food.getProperties() : Drink.getProperties();
                     XmlParser.update(name, properties, userChoice);
                     break;
-                case 5:
+                case 6:
                     System.out.println("\n- [Remove] Remove consumable -\n");
                     userChoice = Input.getInputString("Choose between food and drink: ", validType);
                     validNames = smartFridge.getFridgeItemNames(userChoice);
